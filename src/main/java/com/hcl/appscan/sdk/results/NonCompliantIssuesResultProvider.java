60acfc73a54c3433eb71ba5bc7fce853b82fba10
Matthew Murphy <murphy@mmurphy.local>
2020-05-27 18:51:57 -0400

    use new CountBySeverity api for getting non-compliant issue counts

--- src/main/java/com/hcl/appscan/sdk/results/NonCompliantIssuesResultProvider.java
+++ src/main/java/com/hcl/appscan/sdk/results/NonCompliantIssuesResultProvider.java
@@ -54,25 +54,32 @@ public class NonCompliantIssuesResultProvider extends CloudResultsProvider {
 				setHasResult(true);
 			} else if (m_status != null && !(m_status.equalsIgnoreCase(INQUEUE) || m_status.equalsIgnoreCase(RUNNING))) {
 				JSONArray array = m_scanProvider.getNonCompliantIssues(m_scanId);
-				m_totalFindings = array.length();
+				m_totalFindings = 0;
+				
 				for (int i = 0; i < array.length(); i++) {
 					JSONObject jobj = array.getJSONObject(i);
 					String sev = jobj.getString("Severity");
+					int count = jobj.getInt("Count");
+					
 					switch (sev.toLowerCase()) {
 					case "high":
-						m_highFindings++;
+						m_highFindings = count;
+						m_totalFindings += count;
 						break;
 					case "medium":
-						m_mediumFindings++;
+						m_mediumFindings = count;
+						m_totalFindings += count;
 						break;
 					case "low":
-						m_lowFindings++;
+						m_lowFindings = count;
+						m_totalFindings += count;
 						break;
 					case "informational":
-						m_infoFindings++;
+						m_infoFindings = count;
+						m_totalFindings += count;
 						break;
-
 					default:
+						m_totalFindings += count;
 						break;
 					}
 				}
